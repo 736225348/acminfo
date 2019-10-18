5cf4cabcd6d84b636aeaab9dcb5aa2e8d2919d38
luo <736225348@qq.com>
2019-10-18 14:10:25 +0800

    在 控制器增加了一个方法

--- src/main/java/com/dqsy/algorithmhome/user/controller/UserController.java
+++ src/main/java/com/dqsy/algorithmhome/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.dqsy.algorithmhome.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,5 +10,11 @@ public class UserController {
     @RequestMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/login2")
+    public String login2() {
+        return "login2";
+
     }
 }
